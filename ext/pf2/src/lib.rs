cec37d04003cfcf192b8bfd49ba8e7249a61791b
Daisuke Aritomo <osyoyu@osyoyu.com>
2023-10-20 21:05:45 -0700

    Configure for development in Rust

---
+++ ext/pf2/src/lib.rs
@@ -0,0 +1,8 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+mod ruby_init;
+
+mod sample_collector;
+mod util;
---
+++ ext/pf2/src/util.rs
@@ -0,0 +1,21 @@
+use core::mem::transmute;
+use rb_sys::*;
+
+// Convert str literal to C string literal
+macro_rules! cstr {
+    ($s:expr) => {
+        concat!($s, "\0").as_ptr() as *const std::ffi::c_char
+    };
+}
+pub(crate) use cstr;
+
+pub type RubyCFunc = unsafe extern "C" fn() -> VALUE;
+
+// TODO: rewrite as macro
+pub fn to_ruby_cfunc1<T>(f: unsafe extern "C" fn(T) -> VALUE) -> RubyCFunc {
+    unsafe { transmute::<unsafe extern "C" fn(T) -> VALUE, RubyCFunc>(f) }
+}
+// TODO: rewrite as macro
+pub fn to_ruby_cfunc2<T, U>(f: unsafe extern "C" fn(T, U) -> VALUE) -> RubyCFunc {
+    unsafe { transmute::<unsafe extern "C" fn(T, U) -> VALUE, RubyCFunc>(f) }
+}
