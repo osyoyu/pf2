ec05f9b4026f8be68eb5a26cea15b74f40155762
Daisuke Aritomo <osyoyu@osyoyu.com>
2023-11-23 22:49:30 +0900

    Pf2.start, Pf2.stop

--- lib/pf2.rb
+++ lib/pf2.rb
@@ -4,7 +4,21 @@ require_relative 'pf2/version'
 module Pf2
   class Error < StandardError; end
 
+  @@collector = nil
   @@threads = []
+
+  def self.start(...)
+    @collector = Pf2::TimerCollector.new
+    @collector.start(...)
+  end
+
+  def self.stop
+    @collector.stop
+  end
+
+  def self.install_to_current_thread(...)
+    @collector.install_to_current_thread(...)
+  end
 
   def self.threads
     @@threads
