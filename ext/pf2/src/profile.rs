699acac112cea6a6d53df53756af132f49936f70
Daisuke Aritomo <osyoyu@osyoyu.com>
2024-01-20 21:07:23 +0900

    Mini optimization: Keep known VALUEs in a HashSet

--- ext/pf2/src/profile.rs
+++ ext/pf2/src/profile.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::time::Instant;
 
 use rb_sys::*;
@@ -10,6 +11,7 @@ pub struct Profile {
     pub start_timestamp: Instant,
     pub samples: Vec<Sample>,
     pub temporary_sample_buffer: Ringbuffer,
+    known_values: HashSet<VALUE>,
 }
 
 impl Profile {
@@ -18,19 +20,27 @@ impl Profile {
             start_timestamp: Instant::now(),
             samples: vec![],
             temporary_sample_buffer: Ringbuffer::new(10000),
+            known_values: HashSet::new(),
         }
     }
 
     pub fn flush_temporary_sample_buffer(&mut self) {
         while let Some(sample) = self.temporary_sample_buffer.pop() {
+            self.known_values.insert(sample.ruby_thread);
+            for frame in sample.frames.iter() {
+                if frame == &0 {
+                    break;
+                }
+                self.known_values.insert(*frame);
+            }
             self.samples.push(sample);
         }
     }
 
     pub unsafe fn dmark(&self) {
-        self.samples.iter().for_each(|sample| {
-            sample.dmark();
-        });
+        for value in self.known_values.iter() {
+            rb_gc_mark(*value);
+        }
         self.temporary_sample_buffer.dmark();
     }
 }
