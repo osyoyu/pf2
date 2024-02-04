e90ba93bcf9187acff13c711bdbdc802a395e0e3
Daisuke Aritomo <osyoyu@osyoyu.com>
2024-02-05 00:42:37 +0900

    Relax test timeout

--- test/script_completion_test.rb
+++ test/script_completion_test.rb
@@ -4,14 +4,14 @@ require_relative './test_helper.rb'
 
 class ScriptCompletionTest < Minitest::Test
   def test_mandelbrot_in_main_thread_completes
-    assert_equal(true, system_with_timeout('bundle exec ruby test/test_scripts/mandelbrot_in_main_thread.rb', 5).success?)
+    assert_equal(true, system_with_timeout('bundle exec ruby test/test_scripts/mandelbrot_in_main_thread.rb', 10).success?)
   end
 
   def test_mandelbrot_in_new_thread_completes
-    assert_equal(true, system_with_timeout('bundle exec ruby test/test_scripts/mandelbrot_in_new_thread.rb', 5).success?)
+    assert_equal(true, system_with_timeout('bundle exec ruby test/test_scripts/mandelbrot_in_new_thread.rb', 10).success?)
   end
 
   def test_mandelbrot_in_multiple_threads_completes
-    assert_equal(true, system_with_timeout('bundle exec ruby test/test_scripts/mandelbrot_in_multiple_threads.rb', 8).success?)
+    assert_equal(true, system_with_timeout('bundle exec ruby test/test_scripts/mandelbrot_in_multiple_threads.rb', 15).success?)
   end
 end
