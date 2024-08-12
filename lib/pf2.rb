osyoyu/pf2

commit 95b2edb8918c0197e0f4ae44829532f35d5c064c

Clean up after `Pf2.profile`

lib/pf2.rb
@@ -19,5 +19,6 @@ module Pf2
     start(threads: Thread.list)
     yield
     stop
+    @@session = nil # let GC clean up the session
   end
 end

commit 8c9a9b775fba7e6164925ad2445761f97ce2aa58

fix: Return profile result from Pf2.profile 95b2edb8918c0197e0f4ae44829532f35d5c064c broke the return value of Pf2.profile, which is cited as an example in the README.md.

lib/pf2.rb
@@ -18,7 +18,8 @@ module Pf2
     raise ArgumentError, "block required" unless block_given?
     start(threads: Thread.list)
     yield
-    stop
+    result = stop
     @@session = nil # let GC clean up the session
+    result
   end
 end