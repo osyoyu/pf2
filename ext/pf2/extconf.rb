b5c127eead6d1b208684e6f6c3592388b78bfef0
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-01-05 02:05:06 +0900

    force false

--- ext/pf2/extconf.rb
+++ ext/pf2/extconf.rb
@@ -7,7 +7,5 @@ create_rust_makefile 'pf2/pf2' do |r|
   if ENV['PF2_FEATURES']
     r.features = ENV['PF2_FEATURES'].split(",")
   end
-  if ENV['PF2_AUTO_INSTALL_RUST_TOOLCHAIN']
-    r.auto_install_rust_toolchain = (["1", "true"].include?(ENV['PF2_AUTO_INSTALL_RUST_TOOLCHAIN']))
-  end
+  r.auto_install_rust_toolchain = false
 end
