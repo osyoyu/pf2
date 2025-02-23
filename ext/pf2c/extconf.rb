5b7527768a9fdea73e5e6d7135e7bd07088cc32f
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-02-24 01:54:25 +0900

    Check if timer_create is available in extconf

--- ext/pf2c/extconf.rb
+++ ext/pf2c/extconf.rb
@@ -3,5 +3,7 @@ require 'mkmf'
 append_cflags('-fvisibility=hidden')
 append_cflags('-DPF2_DEBUG') # TODO: make this conditional
 
-$srcs = Dir.glob("#{File.join(File.dirname(__FILE__), '*.c')}")
-create_makefile 'pf2/pf2c'
+if have_func('timer_create')
+  $srcs = Dir.glob("#{File.join(File.dirname(__FILE__), '*.c')}")
+  create_makefile 'pf2/pf2c'
+end
