0b29531290bd12ff3156455291b3639b821eb1ff
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-03-02 01:59:31 +0900

    Capture native stack frames (PCs)

--- ext/pf2c/sample.c
+++ ext/pf2c/sample.c
@@ -1,10 +1,17 @@
 #include <stdbool.h>
 #include <time.h>
 
+#include <backtrace.h>
 #include <ruby.h>
 #include <ruby/debug.h>
 
+#include "backtrace_state.h"
 #include "sample.h"
+
+const int PF2_SAMPLE_MAX_NATIVE_DEPTH = 300;
+
+static int capture_native_backtrace(struct pf2_sample *sample);
+static int backtrace_on_ok(void *data, uintptr_t pc);
 
 // Capture a sample from the current thread.
 bool
@@ -18,5 +25,46 @@ pf2_sample_capture(struct pf2_sample *sample)
     // Obtain the current stack from Ruby
     sample->depth = rb_profile_frames(0, 200, sample->cmes, sample->linenos);
 
+    // Capture C-level backtrace
+    sample->native_stack_depth = capture_native_backtrace(sample);
+
     return true;
+}
+
+// Struct to be passed to backtrace_on_ok
+struct bt_data {
+    struct pf2_sample *pf2_sample;
+    int index;
+};
+
+static int
+capture_native_backtrace(struct pf2_sample *sample)
+{
+    struct backtrace_state *state = global_backtrace_state;
+    assert(state != NULL);
+
+    struct bt_data data;
+    data.pf2_sample = sample;
+    data.index = 0;
+
+    // Capture the current PC
+    // Skip the first 3 frames (capture_native_backtrace, sigprof_handler, <unknown> ?)
+    backtrace_simple(state, 2, backtrace_on_ok, pf2_backtrace_print_error, &data);
+
+    return data.index;
+}
+
+static int
+backtrace_on_ok(void *data, uintptr_t pc)
+{
+    struct bt_data *bt_data = (struct bt_data *)data;
+    struct pf2_sample *sample = bt_data->pf2_sample;
+
+    // Store the PC value
+    if (bt_data->index < PF2_SAMPLE_MAX_NATIVE_DEPTH) {
+        sample->native_stack[bt_data->index] = pc;
+        bt_data->index++;
+    }
+
+    return 0; // Continue backtrace
 }
--- ext/pf2c/sample.h
+++ ext/pf2c/sample.h
@@ -3,10 +3,16 @@
 
 #include <ruby.h>
 
+extern const int PF2_SAMPLE_MAX_NATIVE_DEPTH;
+
 struct pf2_sample {
     int depth;
     VALUE cmes[200];
     int linenos[200];
+
+    size_t native_stack_depth;
+    uintptr_t native_stack[200];
+
     uint64_t consumed_time_ns;
     uint64_t timestamp_ns;
 };
