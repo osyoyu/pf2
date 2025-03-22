fad6f7eaeb4f0a6293b1947a8639a52f6bfb6219
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-03-22 16:12:55 +0900

    Add profiling_test

---
+++ test/profiling_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative './test_helper.rb'
+
+require 'pf2'
+
+def loop_addition
+  1000000.times do
+    (+"foo") + (+"bar")
+  end
+end
+
+class ProfilingTest < Minitest::Test
+  def test_profiling_loop_addition
+    return if !defined?(Pf2c)
+
+    session = Pf2c::Session.new
+    session.start
+    loop_addition
+    profile = session.stop
+
+    assert_operator profile[:samples].size, :>=, 10, "Expected at least 10 samples"
+    assert_operator profile[:samples].size, :<, 20, "Expected no more than 20 samples"
+  end
+end
